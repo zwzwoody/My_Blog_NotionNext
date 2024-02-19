4ae34a4aad60acf36f6b320aea6765178299fc02
zwzwoody <649499349@qq.com>
2024-02-20 02:17:19 +0800

    Merge remote-tracking branch 'origin/main'

--- blog.config.js
+++ blog.config.js
@@ -214,7 +214,7 @@ const BLOG = {
     {
       name: '风を共に舞う気持ち',
       artist: 'Falcom Sound Team jdk',
-      url: 'https://music.163.com/song/media/outer/url?id=731419.mp3',
+      url: 'https://music.163.com/song/media/outer/url?id=484056997.mp3',
       cover:
             'https://p2.music.126.net/kn6ugISTonvqJh3LHLaPtQ==/599233837187278.jpg'
     },
