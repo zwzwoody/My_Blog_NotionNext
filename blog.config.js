315025f977275c662e76ea31746e4e8c84e5536d
Hidragon <141896804+zwzwoody@users.noreply.github.com>
2024-02-21 11:27:35 +0800

    新的

--- blog.config.js
+++ blog.config.js
@@ -1,5 +1,5 @@
 // 注: process.env.XX是Vercel的环境变量，配置方式见：https://docs.tangly1024.com/article/how-to-config-notion-next#c4768010ae7d44609b744e79e2f9959a
-// 2.21  对失效的音源进行更改2
+// 2.21  对失效的音源进行更改222
 const BLOG = {
   // Important page_id！！！Duplicate Template from  https://www.notion.so/tanghh/02ab3b8678004aa69e9e415905ef32a5
   NOTION_PAGE_ID:
